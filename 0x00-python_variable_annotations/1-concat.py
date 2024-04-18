f71fe9ec2d8f1a48e87b1d7de8034273b752d01b
StevenAwo <rotimiawomodu@gmail.com>
2024-04-18 17:49:16 +0100

    TASK 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' Description: this takes the string 'str1' and the string 'str2' as arguments
+    and then returns it as a concatenated string
+        parameters: str1: str
+                    str2: str
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' Returning the concatenation of the strings str1 and str2. '''
+    return str1 + str2
