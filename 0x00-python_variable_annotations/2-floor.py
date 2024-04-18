573c70fd6f0c38f9b51a2d4f12ecc1b612aba439
StevenAwo <rotimiawomodu@gmail.com>
2024-04-18 21:17:28 +0100

    TASK 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 ''' Description:takes in a float n as an argument and then returns
     the floor of the float
-    Parameter: numb: float
+    Parameter: x: float
 '''
 
 
-def floor(nunb: float) -> int:
+def floor(x: float) -> int:
     ''' Return's the largest int value thats less than or equal to n. '''
-    return int(numb) if numb >= 0 else int(numb) - 1
+    return int(x) if x >= 0 else int(x) - 1
