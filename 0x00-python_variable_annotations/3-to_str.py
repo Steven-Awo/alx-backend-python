2618d326007f0248301a2e35ec63c256b91b2a84
StevenAwo <rotimiawomodu@gmail.com>
2024-04-18 18:22:42 +0100

    TASK 3

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-''' Description:accepts in a float n to be an argument and returns the string that's a
-    representation of the float.
+''' Description:accepts in a float n to be an argument and returns
+    the string that's a representation of the float.
     Parameters: n: float
 '''
 
