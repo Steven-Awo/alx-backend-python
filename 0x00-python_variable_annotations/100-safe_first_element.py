670e939375e2898048b294ea3930175ee1ef05ba
StevenAwo <rotimiawomodu@gmail.com>
2024-04-19 00:38:28 +0100

    TASK 10

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-''' Description: Build upon the following code with the correct
-                 duck-typed annotations
+''' Description: To build upon just the following code with the corrects
+                 duck-typed's annotations
     Parameters: lst: Sequence[Any]
 '''
 
-from typing import Sequence, Any, Union
+from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    ''' Outputs the first element of lst if there is any, otherwise None. '''
+    ''' Outputing the first element thats of lst if there's
+        any, otherwise None. '''
     if lst:
         return lst[0]
     else:
