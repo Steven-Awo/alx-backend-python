4ec3512add8b8d86cd399fa3ffb35f2f81392754
StevenAwo <rotimiawomodu@gmail.com>
2024-04-22 19:52:22 +0100

    TASK 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-''' Description: This takes the code from wait_n and alter it into
-    a new function task_wait_n. The code is nearly identical
-    to wait_n except task_wait_random is being called.
+
+''' Description: Take the code from wait_n and alter it into a new function
+                 task_wait_n. The code is nearly identical to wait_n except
+                 task_wait_random is being called.
     Arguments: n: int, max_delay: int = 10
 '''
 
