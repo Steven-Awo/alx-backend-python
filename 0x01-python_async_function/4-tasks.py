b96542cef76265519099cf00406cd33fbc937e81
StevenAwo <rotimiawomodu@gmail.com>
2024-04-22 20:01:33 +0100

    TASK 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -16,15 +16,15 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
-    '''Execute task_wait_random and returns sorted list of delay'''
-    spawn_ls = []
-    delay_ls = []
-    for i in range(n):
-        delayed_task = task_wait_random(max_delay)
-        delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
-        spawn_ls.append(delayed_task)
+    '''Executing the task_wait_random and then to returns the sorted list of all the delay'''
+    spawnn_ls = []
+    delayy_ls = []
+    for a in range(n):
+        delayedd_task = task_wait_random(max_delay)
+        delayedd_task.add_done_callback(lambda x: delayy_ls.append(x.result()))
+        spawnn_ls.append(delayedd_task)
 
-    for spawn in spawn_ls:
-        await spawn
+    for spawnr in spawnn_ls:
+        await spawnr
 
-    return delay_ls
+    return delayy_ls
