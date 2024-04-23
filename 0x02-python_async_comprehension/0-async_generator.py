f734bfc04adf7506725a2909aeab08a5668ddaa9
StevenAwo <rotimiawomodu@gmail.com>
2024-04-23 19:16:00 +0100

    TASK 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -11,9 +11,10 @@ from typing import Generator
 
 from random import random
 
+
 async def async_generator() -> Generator[float, None, None]:
-    ''' A generator that does yields a random value thats
-    between 0 and 10 for every second, occuring 10 times. '''
-    for a in range(10):
+    ''' Generator that yields a random value between 0 and 10 every second,
+        10 times. '''
+    for i in range(10):
         await sleep(1)
         yield 10 * random()
