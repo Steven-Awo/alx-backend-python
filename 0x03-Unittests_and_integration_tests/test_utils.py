db8489c762f16da1f169411d87d115cf6794555d
StevenAwo <rotimiawomodu@gmail.com>
2024-05-10 14:51:38 +0100

    TestUtils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,19 +2,20 @@
 """ Testing the SUITE Unittest that module the Task """
 
 from unittest import TestCase, mock
-from unittest.mock import patch, Mock
+
 from parameterized import parameterized
-from utils import access_nested_map, get_json, memoize
+
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(TestCase):
-    """ Class for testing Nested Map function """
+    """ Classng for the testing of the Nested Map's function """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, map, path, expected_output):
-        """ Test method return output """
-        real_output = access_nested_map(map, path)
-        self.assertEqual(real_output, expected_output)
+        """ Testing the methods that returns output """
+        realist_output = access_nested_map(map, path)
+        self.assertEqual(realist_output, expected_output)
