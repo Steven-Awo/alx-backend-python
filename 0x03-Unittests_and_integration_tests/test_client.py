c79615fab2c95f143fb74ac040bc373907de194c
StevenAwo <rotimiawomodu@gmail.com>
2024-05-13 11:16:32 +0100

    TestClient.py

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Testing for the utils for client """
+
+
+import requests
+
+import unittest
+
+from unittest.mock import patch, Mock, PropertyMock, call
+
+from parameterized import parameterized, parameterized_class
+
+import utils
+
+from utils import access_nested_map, get_json, memoize
+
+from client import GithubOrgClient
+
+import client
+
+from fixtures import TEST_PAYLOAD
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Testing so that the json can actually be gotten """
+
+    @parameterized.expand([
+        ("google", {"google": True}),
+        ("abc", {"abc": True})
+    ])
+    @patch('client.get_json')
+    def test_org(self, org, expected, get_patch):
+        """ Testing for the org thats of the client """
+        get_patch.return_value = expected
+        c = GithubOrgClient(org)
+        self.assertEqual(c.org, expected)
+        get_patch.assert_called_once_with("https://api.github.com/orgs/"+org)
