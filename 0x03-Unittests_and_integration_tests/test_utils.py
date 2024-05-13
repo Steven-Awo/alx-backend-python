4fc67b02005eeba72c74b9b8a05e8823298ffe05
StevenAwo <rotimiawomodu@gmail.com>
2024-05-13 11:27:19 +0100

    TestUtils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,7 +7,7 @@ from parameterized import parameterized
 
 from unittest.mock import patch, Mock
 
-from utils import access_nested_map
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(TestCase):
